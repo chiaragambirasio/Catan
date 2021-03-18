04dd8b3d9e762e6c4fd2e5e9256531e380c18481
Chiara <chiara.gambirasio@web.de>
2021-03-18 16:17:45 +0100

    Throw n dices

--- Die.java
+++ Die.java
@@ -14,24 +14,40 @@ public class Die {
         this.number = getRandomNumberUsingNextInt(1,7);
     }
 
-    public int getRandomNumberUsingNextInt(int min, int max){
+    public static int getRandomNumberUsingNextInt(int min, int max){
 
         Random random = new Random();
         return random.nextInt(max-min) + min;
     }
 
+    public static int throwTwoDice(){
 
+        Die die1 = new Die();
+        Die die2 = new Die();
+        int sum = die1.number + die2.number;
+        return sum;
+    }
+
+    public static int throwNDice(int n){
+
+        System.out.println(n);
+        int dicesum = 0;
+
+        for(int i = 0; i <= n; i++){
+
+            Die current = new Die();
+            dicesum += current.number;
+        }
+
+        return dicesum;
+    }
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("How many dice?");
 
-        Die die = new Die();
-        System.out.println("Zahl:" + die.number);
+        int sum = throwNDice(getRandomNumberUsingNextInt(0,1000));
 
-    
-
-        
-
+        System.out.println("Your dicesum:" + sum);
     } 
 
 }
